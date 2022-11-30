d5e6c2839f742bcda4b8b145401d42977b52587d
Julzate <87589813+Julzate@users.noreply.github.com>
2022-11-30 17:19:18 -0500

    added dropdown menu for mobile footer

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+/* Mobile menu button for navbar */
 const btn = document.getElementById("menu-btn")
 const nav = document.getElementById("menu")
 
@@ -8,3 +10,24 @@ function navToggle(){
 }
 
 btn.addEventListener('click', navToggle)
+
+/* Mobile footer dropdown menu */
+
+document.addEventListener("click", e => {
+    const isDropdownTrigger = e.target.matches("[data-dropdown-trigger")
+
+    if(!isDropdownTrigger && e.target.closest("[data-dropdown]")) return
+
+    let currentDropdown
+
+    if (isDropdownTrigger) {
+        currentDropdown = e.target.closest("[data-dropdown]")
+        currentDropdown.classList.toggle("active")
+    }
+
+    document.querySelectorAll("[data-dropdown].active").forEach(dropdown => {
+        if (dropdown === currentDropdown) return
+        dropdown.classList.remove("active")
+    })
+
+})
